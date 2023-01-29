8ed176e8cd10d6579c3930accdadd8e526cc42df
kshreeram <vtu16194@veltech.edu.in>
2023-01-29 16:10:31 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,26 @@
+//  let li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = "Hellow";
+// li[2].style.fontWeight = "bold";
+// li[2].style.backgroundColor = "green";
+
+// for(let i=0;i<li.length;i++)
+// {
+//     li[i].style.fontWeight = "bold";
+// }  
+
+
+let item = document.getElementsByClassName("list-group-item");
+console.log(item);
+console.log(item[1]);
+item[1].textContent = "Hellow";
+item[2].style.fontWeight = "bold";
+item[2].style.backgroundColor = "green";
+
+for(let i=0;i<item.length;i++)
+{
+    item[i].style.fontWeight = "bold";
+}  
+
+
